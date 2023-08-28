9554161d717e14361521def52c7238456f5847b6
Alex Wiehl <alexwiehl@gmail.com>
2023-08-28 18:28:14 -0400

    Add Thunks

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store.js";
 
-store.dispatch({ type: "account/deposit", payload: 530 });
+store.dispatch({ type: "account/deposit", payload: 0 });
 console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
